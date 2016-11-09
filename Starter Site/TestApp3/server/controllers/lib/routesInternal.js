20604364fdd05baddb99b31390b6cb4cd31a7828
Walter Erquinigo <a20012251@gmail.com>
2016-11-09 00:27:47 -0800

    fix flow on the server side

--- Starter Site/TestApp3/server/controllers/lib/routesInternal.js
+++ Starter Site/TestApp3/server/controllers/lib/routesInternal.js
@@ -1,12 +1,14 @@
 /* @flow */
 
 let app = null;
+import invariant from 'invariant';
 
-export function initRoutes(_app): void {
+export function initRoutes(_app: any): void {
   app = _app;
 }
 
-export function get(path, cb) {
+export function get(path: string, cb: any): void {
+  invariant(app != null, "app can't be null");
   app.get(path, (req, res) => {
     res.send(cb(req, res));
   });
