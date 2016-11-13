dc145225859094cba312b1f68b5e4e57cf5412c9
kyle hamilton <kyleih@optonline.net>
2016-11-12 22:50:51 -0500

    add html files

---
+++ HTML/js/graph.js
@@ -0,0 +1,105 @@
+
+
+var width  = 425,
+    height = 300,
+    colors = d3.scale.category10();
+
+if(window.location.host.indexOf('127.0.0.1') != -1){
+            url = 'http://127.0.0.1:8887/';
+        }else{
+            url = 'http://people.ischool.berkeley.edu/~kylehamilton/209/hw4/'
+        }
+
+function draw(elem){
+
+
+	d3.json(url+"data/"+elem+"-data.json", function(error, res) {
+	        if (error){return console.warn(error);}
+	        else{
+	        	drawGraph(res);
+	        }
+	});
+
+	function drawGraph(data){
+
+		// Calculate all the various dimensions of nodes and layers so they can be evenly spaced out
+		var node_H = 40;
+		var node_W = 40;
+		var padding_V = 40;
+		var padding_H = 70;
+
+		layer_dims = []
+		data.map(function(el){
+			layer_dims[el.layer] = layer_dims[el.layer] ?  layer_dims[el.layer]+1 : 1
+		})
+
+		MAXnodes = Math.max.apply(null, layer_dims)
+
+
+		var offset = function(node_count){
+			return (((MAXnodes-1)*(node_H+padding_V)+node_H) - ((node_count-1)*(node_H+padding_V) + node_H )) /2
+		}
+
+		//console.debug('offset',assert(offset(3)==0),assert(offset(1)==60),assert(offset(2)==30))
+		// end dimension calculations
+
+
+
+
+		var svg = d3.select('body').select("#"+elem).append("svg")
+	                    .attr("width", width)
+	                    .attr("height", height);
+
+
+
+
+		var node = svg.selectAll("g")
+	                    .data(data)
+					    .enter()
+					    .append('g')
+					    .attr('class','node')
+					    .attr("transform", "translate("+node_W+", 0)")
+
+		node.append('g')
+					.selectAll('line')
+					.data(function (d) { return d.edges; })
+					.enter()
+					.append('line')
+					.attr("x1", function (d,i){return (d.layer-1)*(node_W + padding_H) })
+					.attr("y1", function (d,i){return d.source*(node_H + padding_V) + offset(layer_dims[d.layer-1]) })
+					.attr("x2", function (d,i){return d.layer*(node_W + padding_H) })
+					.attr("y2", function (d,i){return d.dest*(node_H + padding_V)  + offset(layer_dims[d.layer])})
+					.attr("stroke-width", 1)
+					.attr("stroke", "#cccccc");
+
+
+		node.append('circle')
+					.attr("cx", function (d,i) { return d.layer * (node_W + padding_H); })
+					.attr("cy", function (d,i) { return d.node * (node_H + padding_V) + offset(layer_dims[d.layer]) ; })
+					.attr("r", node_W/2)
+					.style("fill", "#eeeeee")
+					.on('mouseenter',function(){
+						d3.select(this)
+						.transition()
+						.attr("r",node_W/2+5)
+						.style("fill", "#ff0055")
+					})
+					.on('mouseleave',function(){
+						d3.select(this)
+						.transition()
+						.attr("r",node_W/2)
+						.style("fill", "#eeeeee")
+					})
+
+	}
+
+
+}
+
+
+
+
+
+
+
+
---
+++ HTML/js/script.js
@@ -0,0 +1,46 @@
+$(function(){
+
+  // $.ajax();  <<< CORE METHOD
+  // $('').load();
+  // $.get();
+  // $.post();
+  // $.getScript();
+  // $.getJSON();
+  callPage('#home')
+
+ $(document.body).on('click', '.page' , function(e){
+    e.preventDefault();
+    var pageRef = $(this).find('a').attr('href');
+    callPage(pageRef)
+
+  });
+
+ 
+  function callPage(pageRefInput){
+    var pageName = pageRefInput.split("#");
+    pageName = pageName[1]+".html";
+
+    $.ajax({
+        url: pageName,
+        type: "GET",
+        dataType : 'text',
+        success: function( response ) {
+          $('#content').html(response);
+          draw('LR');
+          draw('NN');
+        },
+        error: function( error ) {
+          console.log('Error loading page', error);
+        },
+        complete: function( xhr, status ) {
+          console.log("Request complete");
+        }
+    });    
+  }
+
+
+  function renderPage(data){
+
+  }
+
+});
