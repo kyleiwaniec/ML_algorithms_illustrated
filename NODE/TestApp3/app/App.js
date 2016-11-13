5bd87dd2d71c497ad37b7ca7a013b89883717805
Walter Erquinigo <a20012251@gmail.com>
2016-11-13 11:19:26 -0800

    add tabs

--- NODE/TestApp3/app/App.js
+++ NODE/TestApp3/app/App.js
@@ -3,18 +3,41 @@
 import React from 'react';
 import Chart from './Chart';
 import {LinRegClient} from './LinRegClient';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
-        <Chart
-          costClient={new LinRegClient()}
-          width={600}
-          height={600}
-          margin={50}
-        />
-      </div>
+      <Tabs>
+        <TabList>
+          <Tab>
+            Linear regression
+          </Tab>
+          <Tab>
+            Single Layer NN
+          </Tab>
+        </TabList>
+        <TabPanel>
+          <div>
+            <Chart
+              costClient={new LinRegClient()}
+              width={600}
+              height={600}
+              margin={50}
+            />
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div>
+            <Chart
+              costClient={new LinRegClient()}
+              width={600}
+              height={600}
+              margin={50}
+            />
+          </div>
+        </TabPanel>
+      </Tabs>
     );
   }
 }
