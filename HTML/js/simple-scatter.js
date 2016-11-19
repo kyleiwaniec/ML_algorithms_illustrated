a799d7ebee0df354c815e0ee6952d43c5220c283
kyle hamilton <kyleih@optonline.net>
2016-11-19 18:43:06 -0500

    example weight height

--- HTML/js/simple-scatter.js
+++ HTML/js/simple-scatter.js
@@ -2,38 +2,38 @@ function scatter(){
 
 
 
-	var margin = {top: 23, right: 30, bottom: 30, left: 20}
+	var margin = {top: 23, right: 30, bottom: 40, left: 40}
 		,width  = 235
 	    ,height = 240
 
 	var data = [
-		{x1:2,x2:2,y:0},
-		{x1:1,x2:3,y:0},
-		{x1:2,x2:4,y:0},
-		{x1:4,x2:2,y:1},
-		{x1:5,x2:5,y:1},
-		{x1:6,x2:6,y:1}
+		{x1:5.5,x2:98,y:0},
+		{x1:5.8,x2:120,y:0},
+		{x1:6.2,x2:168,y:0},
+		{x1:5.5,x2:200,y:1},
+		{x1:5,x2:210,y:1},
+		{x1:5,x2:168,y:1}
 	]
 
 	var x = d3.scaleLinear()
-	              .domain([0, d3.max(data, function(d) { return d["x1"]; }) + 1])
+	              .domain([d3.min(data, function(d) { return d["x2"]; })-20, d3.max(data, function(d) { return d["x2"]; })+20])
 	              .range([ 0, width]);
 	    
 	var y = d3.scaleLinear()
-		      .domain([0, d3.max(data, function(d) { return d["x2"]; }) + 1])
+		      .domain([d3.min(data, function(d) { return d["x1"]; })-.1, d3.max(data, function(d) { return d["x1"]; })+.1])
 		      .range([ height, 0 ]);
 
 
 	// gridlines in x axis function
 	function make_x_gridlines() {		
 	    return d3.axisBottom(x)
-	        .ticks(6)
+	        .ticks(10)
 	}
 
 	// gridlines in y axis function
 	function make_y_gridlines() {		
 	    return d3.axisLeft(y)
-	        .ticks(6)
+	        .ticks(5)
 	}
 
 	var chart = d3.select('#Scatter')
@@ -65,16 +65,16 @@ function scatter(){
 		chart.append("g")
 		  .attr("transform", "translate(" + margin.left + "," + (height + margin.top) + ")")
 		  .call(d3.axisBottom(x)
-		  	.tickValues([0, 1, 2, 3, 4, 5, 6, 7])
-		  	.tickFormat(d3.format(",.0f"))
+		  	// .tickValues([0, 1, 2, 3, 4, 5, 6, 7])
+		  	// .tickFormat(d3.format(",.0f"))
 		  	)
 
 		// add the Y Axis
 		chart.append("g")
 		  .attr("transform", "translate(" + margin.left + "," + (margin.top) + ")")
 		  .call(d3.axisLeft(y)
-		  	.tickValues([0, 1, 2, 3, 4, 5, 6, 7])
-		  	.tickFormat(d3.format(",.0f"))
+		  	// .tickValues([0, 1, 2, 3, 4, 5, 6, 7])
+		  	// .tickFormat(d3.format(",.0f"))
 		  	);
 		
 
@@ -90,14 +90,14 @@ function scatter(){
 	      .data(data)
 	      .enter().append("svg:circle")
 	      	  .attr("class",function (d){return "data-point-"+d["y"]})
-	          .attr("cx", function (d,i) { return x(d["x1"]); } )
-	          .attr("cy", function (d) { return y(d["x2"]); } )
+	          .attr("cx", function (d,i) { return x(d["x2"]); } )
+	          .attr("cy", function (d) { return y(d["x1"]); } )
 	          .attr("r", 6)
 
 	      .on("mouseenter",function(d,i){
 	      	console.log("d",d)
-	      	var xPosition = parseFloat(d3.select(this).attr("cx"))+10;
-            var yPosition = parseFloat(d3.select(this).attr("cy"))-20;
+	      	var xPosition = parseFloat(d3.select(this).attr("cx"))-10;
+            var yPosition = parseFloat(d3.select(this).attr("cy"))-30;
 
             //Update the tooltip position and value
             var tooltip = d3.select("#tooltip")
@@ -105,10 +105,10 @@ function scatter(){
               .style("top", yPosition + "px")
               
             tooltip.select("#x1")
-              .text("$x_{i1} = "+d['x1']+"$")
+              .text("$x_{i1} = "+d['x1']+" ft$")
               
             tooltip.select("#x2")
-              .text("$x_{i2} = "+d['x2']+"$")
+              .text("$x_{i2} = "+d['x2']+" lbs$")
 
             tooltip.select("#y")
               .text("$y = "+d['y']+"$")
@@ -123,6 +123,17 @@ function scatter(){
                d3.select("#tooltip").classed("hidden",true);   
                }); 
 
+	      // now add titles to the axes
+        main.append("text")
+        	.attr("class","math-font")
+            .attr("text-anchor", "middle")  // this makes it easy to centre the text as the transform is applied to the anchor
+            .attr("transform", "translate("+ (-margin.left*.75) +","+(height/2)+")rotate(-90)")  // text is drawn off the screen top left, move down and out and rotate
+            .text("Height in ft");
 
+        main.append("text")
+        	.attr("class","math-font")
+            .attr("text-anchor", "middle")  // this makes it easy to centre the text as the transform is applied to the anchor
+            .attr("transform", "translate("+ (width/2) +","+(height+(margin.bottom*.95))+")")  // centre below axis
+            .text("Weight in lbs");
 }
 
