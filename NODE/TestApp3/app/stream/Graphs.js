89a5a289938802623138dfbbdb576c1d28c7e0fa
Walter Erquinigo <a20012251@gmail.com>
2016-11-20 11:06:19 -0800

    add a graph

--- NODE/TestApp3/app/stream/Graphs.js
+++ NODE/TestApp3/app/stream/Graphs.js
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom';
 
 import {MnistStream} from './MnistStream';
 import type {Iteration} from './MnistStream';
+import {Line} from 'react-chartjs-2';
 
 type Props = {
   stream: ?MnistStream,
@@ -52,19 +53,41 @@ export class Graphs extends React.Component {
   }
 
   render(): React.Element<any> {
+    if (this.state.iterations.length === 0) {
+      return (<div />);
+    }
     return (
       <div>
-        {this.state.finished ? <div>Finished</div> : null}
-        <div>{this.state.iterations.length} Iterations</div>
-        <ul>
-          {this.state.iterations.map(iteration => (
-            <li key={iteration.index}>
-              <div>
-                Error: {iteration.error}
-              </div>
-            </li>
-          ))}
-        </ul>
+        <div>{this.state.finished ? 'Finished' : null}</div>
+        <Line
+          options={{scales: {xAxes: [{display: false}]}}}
+          data={{
+            labels: new Array(this.state.iterations.length).fill().map((x, i) => 'It. ' + i),
+            datasets: [
+              {
+                label: 'Error',
+                fill: false,
+                lineTension: 0.1,
+                backgroundColor: 'rgba(75,192,192,0.4)',
+                borderColor: 'rgba(75,192,192,1)',
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: 'rgba(75,192,192,1)',
+                pointBackgroundColor: '#fff',
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                pointHoverBorderColor: 'rgba(220,220,220,1)',
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: this.state.iterations.map(it => it.error),
+              },
+            ]
+          }}
+        />
       </div>
     );
   }
