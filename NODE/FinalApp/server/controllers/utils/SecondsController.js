20698874799f58e1fa2721fe6a197c28a52c9038
Walter Erquinigo <a20012251@gmail.com>
2016-11-23 23:55:09 -0800

    remove

--- NODE/FinalApp/server/controllers/utils/SecondsController.js
+++
@@ -1,9 +0,0 @@
-/* @flow */
-
-import {Controller} from '../lib/Controller.js'
-
-export class SecondsController extends Controller {
-  getCurrentSecondsAsString(): string {
-    return (new Date()).getSeconds().toString();
-  }
-}
