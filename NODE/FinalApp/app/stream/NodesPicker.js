491d180a50f856c4c932e8eae13bc3a3a10b9512
Walter Erquinigo <a20012251@gmail.com>
2016-11-23 21:22:28 -0800

    copy

---
+++ NODE/FinalApp/app/stream/NodesPicker.js
@@ -0,0 +1,98 @@
+/* @flow */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Button, DropdownButton, MenuItem} from 'react-bootstrap';
+
+type State = {
+  nodes: Array<number>,
+};
+
+const btnStyle = {margin: '2px 2px 2px 2px'};
+const btnType = 'default';
+const defaultNodes = 5;
+
+export class NodesPicker extends React.Component {
+  state: State;
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      nodes: [10],
+    };
+  }
+
+  render(): React.Element<any> {
+    return (
+      <div>
+        {this.state.nodes.map((node, idx) => this.renderNode(node, idx))}
+        <Button
+          bsSize="small"
+          bsStyle={btnType}
+          style={btnStyle}
+          onClick={() => this.addOne()}>
+          +
+        </Button>
+        <Button
+          bsSize="small"
+          bsStyle={btnType}
+          style={btnStyle}
+          onClick={() => this.removeOne()}>
+          -
+        </Button>
+      </div>
+    );
+  }
+
+  getNodes(): Array<number> {
+    return this.state.nodes;
+  }
+
+  addOne(): void {
+    const nodes = this.state.nodes.slice();
+    nodes.push(defaultNodes);
+    this.setState({nodes});
+  }
+
+  removeOne(): void {
+    if (this.state.nodes.length === 1) {
+      return;
+    }
+    const nodes = this.state.nodes.slice();
+    nodes.pop();
+    this.setState({nodes});
+
+  }
+
+  onChange(idx: number, value: number): void {
+    const nodes = this.state.nodes.slice();
+    nodes[idx] = value;
+    this.setState({nodes});
+  }
+
+  renderNode(node: number, idx: number): React.Element<any> {
+    return (
+      <DropdownButton
+        key={`selector-${idx}`}
+        title={node}
+        id={`selector-${idx}`}
+        bsStyle={btnType}
+        bsSize="small"
+        style={btnStyle}
+        >
+        {new Array(10).fill().map((_, i_) => {
+          const i = i_ + 1;
+          return (
+            <MenuItem
+              eventKey={i}
+              key={i}
+              onClick={() => this.onChange(idx, i)}
+              active={i === node}>
+              {i}
+            </MenuItem>
+          );
+        })}
+      </DropdownButton>
+    )
+  }
+}
---
+++ NODE/FinalApp/server/controllers/mnist/MnistController.js
@@ -0,0 +1,34 @@
+/* @flow */
+
+import {StreamController} from '../lib/StreamController';
+import brain from '../../brain/lib/neuralnetwork';
+import fs from 'fs';
+import mnist from 'mnist';
+
+export class MnistController extends StreamController {
+  run(): void {
+    const nodes = this.req.query.nodes.split(' ');
+    console.log('Using ' + nodes.join(' ') + ' nodes');
+    const net = new brain.NeuralNetwork({
+      hiddenLayers: nodes,
+    });
+    const set = mnist.set(5000, 0);
+    const trainingSet = set.training;
+    //const testSet = set.test;
+
+    net.train(trainingSet,
+      {
+        errorThresh: 0.05,  // error threshold to reach
+        iterations: 200,   // maximum training iterations
+        log: true,           // console.log() progress periodically
+        logPeriod: 1,       // number of iterations between logging
+        learningRate: 0.3,    // learning rate
+        callback: data =>  {
+          this._see.send(data);
+        },
+        callbackPeriod: 1
+      }
+    );
+    this._see.send('done');
+  }
+}
